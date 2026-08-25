e620bfb2372ed27151aadb8c718e2de6bf5a509a
yogita-GLBITM <25113cn283@glbitm.ac.in>
2026-08-25 19:40:17 +0530

    LC-permutations [Runtime: N/A; Memory: N/A]

--- backtracking/permutations.cpp
+++ backtracking/permutations.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-     vector<vector<int>>result;   
-     backtrack(nums,0,result);
+     vector<vector<int>>result;  //blank notebook called result everytime new arrangement stored in it 
+     backtrack(nums,0,result); //backtrack fun start with pos 0 giving it in the result notebook
      return result;
     }
     private:
-    void backtrack(vector<int>&nums,int start,vector<vector<int>>&result){
+    void backtrack(vector<int>&nums,int start,vector<vector<int>>&result){ //"This is my helper. It needs to know three things: the current state of the numbers, which spot (start) it is currently trying to fill, and the notebook to write in."
+
+C++
         if(start==nums.size()){
             result.push_back(nums);
-            result;
+            result; //base case
         }
         for(int i=start;i<nums.size();i++){
             swap(nums[start],nums[i]);
