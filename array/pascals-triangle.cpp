0538963ea8d0ef812cd194d9ccaf0f2ba4dd241c
yogita-GLBITM <25113cn283@glbitm.ac.in>
2026-08-26 14:17:51 +0530

    LC-pascals-triangle [Runtime: N/A; Memory: N/A]

--- array/pascals-triangle.cpp
+++ array/pascals-triangle.cpp
@@ -4,8 +4,8 @@ public:
         vector<vector<int>>ans;
         
         for(int i=0;i<numRows;i++){
-            vector<int>row(i+1,1);
-            for(int j=1;j<i;j++){
+            vector<int>row(i+1,1);//create row for n elemnets as per the line
+            for(int j=1;j<i;j++){  //calculate middle elements 
                 row[j]=ans[i-1][j-1]+ans[i-1][j];
             }
             ans.push_back(row);
