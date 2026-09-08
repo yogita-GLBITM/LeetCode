f4c240f68bf42179c237e47f627c4371b14a2bc1
yogita-GLBITM <25113cn283@glbitm.ac.in>
2026-09-08 19:02:05 +0530

    LC-merge-two-sorted-lists [Runtime: N/A; Memory: N/A]

--- linked-list/merge-two-sorted-lists.cpp
+++ linked-list/merge-two-sorted-lists.cpp
@@ -8,21 +8,29 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummy(-1);
-        ListNode* tail=&dummy;
-        while(list1!=NULL && list2!=NULL){
-            if(list1->val<list2->val){
-           tail->next=list1;
-list1=list1->next;
+        ListNode* dummy= new ListNode(0);
+        ListNode* tail =dummy;
+        while(list1!=nullptr && list2!=nullptr){
+            if(list1->val <= list2->val){
+                tail->next=list1;
+                list1=list1->next;
             }
             else{
                 tail->next=list2;
                 list2=list2->next;
             }
-              return dummy->next;
+            tail=tail->next;
         }
-    
+        if(list1!=nullptr){
+            tail->next=list1;
+        }
+        else{
+            tail->next=list2;
+        }
+        return dummy->next;
+    }
 };
